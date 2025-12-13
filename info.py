ad1ed1937ca950c6f68ca672e05e6fbf18cc9532
sreeharipradeep365-ui <sreeharipradeep365@gmail.com>
2025-12-13 09:46:58 +0530

    autov3pro.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/')  # Support group lin
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003270499235")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1003341585320")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-1003335865343")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
